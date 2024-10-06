ef808de0ed621ecf56199422bfe8cc6c8e56a3c0
Madara Uchiha <141416632+itzmadara@users.noreply.github.com>
2024-10-06 14:35:03 +0530

    Update __init__.py

--- Mikobot/__init__.py
+++ Mikobot/__init__.py
@@ -171,7 +171,7 @@ else:
 # Add OWNER_ID to the DRAGONS and DEV_USERS sets
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(5907205317)
+DEV_USERS.add(1645068158)
 # <=======================================================================================================>
 
 # <============================================== INITIALIZE APPLICATION =========================================================>
@@ -189,13 +189,13 @@ ALIVE_MSG = """
 """
 
 ALIVE_IMG = [
-    "https://telegra.ph/file/40b93b46642124605e678.jpg",
-    "https://telegra.ph/file/01a2e0cd1b9d03808c546.jpg",
-    "https://telegra.ph/file/ed4385c26dcf6de70543f.jpg",
-    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
-    "https://telegra.ph/file/cce9038f6a9b88eb409b5.jpg",
-    "https://telegra.ph/file/262c86393730a609cdade.jpg",
-    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
+    "https://envs.sh/Bs2.jpg",
+    "https://envs.sh/Bst.jpg",
+    "https://envs.sh/BsK.jpg",
+    "https://envs.sh/Bs5.jpg",
+    "https://envs.sh/BsL.jpg",
+    "https://envs.sh/BsZ.jpg",
+    "https://envs.sh/Bs2.jpg",
 ]
 # <=======================================================================================================>
 
