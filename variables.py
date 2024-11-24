94e72e7f1d93e072ff4e6cc6a498a8fdac723b97
Madara Uchiha <141416632+itzmadara@users.noreply.github.com>
2024-11-25 00:42:48 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -19,7 +19,7 @@ class Config(object):
     API_HASH = "3bc4c3c2416c746fb9d613e205e8a320"
 
     # Database configuration (PostgreSQL)
-    DATABASE_URL = "postgres://koyeb-adm:hpJjgz42wunQ@ep-holy-wildflower-a2hgcv8r.eu-central-1.pg.koyeb.app/koyebdb"
+    DATABASE_URL = "postgresql://Furina_owner:XA5UK7NSFtJo@ep-frosty-bush-a5ljqe50.us-east-2.aws.neon.tech/Furina?sslmode=require"
 
     # Event logs chat ID and message dump chat ID
     EVENT_LOGS =  -1002219420074
