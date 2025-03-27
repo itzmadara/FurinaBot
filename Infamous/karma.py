24f6f9724cd81d555c4c1499b3365115d5c6fd4c
Madara Uchiha <141416632+itzmadara@users.noreply.github.com>
2025-03-27 07:58:40 +0530

    Update karma.py

--- Infamous/karma.py
+++ Infamous/karma.py
@@ -1,7 +1,3 @@
-# https://github.com/Infamous-Hydra/YaeMiko
-# https://github.com/Team-ProjectCodeX
-# https://t.me/O_okarma
-
 # <============================================== IMPORTS =========================================================>
 from pyrogram.types import InlineKeyboardButton as ib
 from telegram import InlineKeyboardButton
@@ -10,31 +6,25 @@ from Mikobot import BOT_USERNAME, OWNER_ID, SUPPORT_CHAT
 
 # <============================================== CONSTANTS =========================================================>
 START_IMG = [
-    "https://envs.sh/Bro.jpg",
-    "https://envs.sh/Br7.jpg",
-    "https://envs.sh/Br8.jpg",
-    "https://envs.sh/Brk.jpg",
-    "https://envs.sh/Brl.jpg",
-    "https://envs.sh/Br4.jpg",
-    "https://envs.sh/Br1.jpg",
+    "https://telegra.ph/file/f426d88230795d1fdf3a1-e6ceb7628d7c82c236.jpg",
+    "https://telegra.ph/file/2c9896c728b9475fe075f-bd65e022804aa17787.jpg",
+    "https://telegra.ph/file/f00924ffe442b1a288c17-b47ded1a63ebeb1fa2.jpg",
+    "https://telegra.ph/file/7a44dfd5e4ca52b2ff578-bdeef004789e9c96cd.jpg",
+    "https://telegra.ph/file/d1cd41d4352c6b4f5b459-4dc463d3ace2e0baa9.jpg",
+    "https://telegra.ph/file/c0f46801453fd1cb7e89f-6f56d5117f3c70fb63.jpg",
+    "https://telegra.ph/file/1624216d188b2ef68f5ed-da54abf587fd4eb8a1.jpg",
 ]
 
-HEY_IMG = "https://envs.sh/BrK.jpg"
+HEY_IMG = "https://telegra.ph/file/ec699b8d507d1a7611067-c1012daccba3ef9dd1.jpg"
 
 ALIVE_ANIMATION = [
-    "https://envs.sh/BrO.jpg",
-    "https://envs.sh/Brf.jpg",
-    "https://envs.sh/Bra.jpg",
-    "https://envs.sh/BrM.jpg",
-    "https://envs.sh/BrX.jpg",
-    "https://envs.sh/Bry.jpg",
-    "https://envs.sh/Brx.jpg",
-    "https://envs.sh/Br-.jpg",
+    "https://telegra.ph/file/68940987fb2fff34d2c32-376015f799ee66aa5b.jpg",
+    "https://telegra.ph/file/ace1442303afb84d3f1c0-10cfaf69755eaeba3c.jpg",
 ]
 
 FIRST_PART_TEXT = "✨ *ʜᴇʟʟᴏ* `{}` . . ."
 
-PM_START_TEXT = "✨ *ɪ ᴀᴍ ꜰᴜʀɪɴᴀ, ᴀ ɢᴇɴꜱʜɪɴ ɪᴍᴘᴀᴄᴛ ᴛʜᴇᴍᴇᴅ ʀᴏʙᴏᴛ ᴡʜɪᴄʜ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴍᴀɴᴀɢᴇ ᴀɴᴅ ꜱᴇᴄᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ʜᴜɢᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ*"
+PM_START_TEXT = "✨ *ɪ ᴀᴍ ʀᴀɪᴅᴇɴ, ᴀ ɢᴇɴꜱʜɪɴ ɪᴍᴘᴀᴄᴛ ᴛʜᴇᴍᴇᴅ ʀᴏʙᴏᴛ ᴡʜɪᴄʜ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴍᴀɴᴀɢᴇ ᴀɴᴅ ꜱᴇᴄᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ʜᴜɢᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ*"
 
 START_BTN = [
     [
@@ -48,7 +38,7 @@ START_BTN = [
     ],
     [
         InlineKeyboardButton(text="DETAILS", callback_data="Miko_"),
-        InlineKeyboardButton(text="Anime", url="https://t.me/AnimeStation2"),
+        InlineKeyboardButton(text="SUPPORT", url="https://t.me/raiden_gc"),
     ],
     [
         InlineKeyboardButton(text="CREATOR", url=f"tg://user?id={OWNER_ID}"),
@@ -71,7 +61,7 @@ GROUP_START_BTN = [
 ALIVE_BTN = [
     [
         ib(text="UPDATES", url="https://t.me/Anime_Station_Bots"),
-        ib(text="SUPPORT", url="https://t.me/+ONpQlt6d_95iNDA1"),
+        ib(text="SUPPORT", url="https://t.me/raiden_gc"),
     ],
     [
         ib(
@@ -82,7 +72,7 @@ ALIVE_BTN = [
 ]
 
 HELP_STRINGS = """
-🫧 *ꜰᴜʀɪɴᴀ* 🫧 [ㅤ](https://envs.sh/Brz.jpg)
+🫧 *ʀᴀɪᴅᴇɴ* 🫧 [ㅤ](https://telegra.ph/file/d1cd41d4352c6b4f5b459-4dc463d3ace2e0baa9.jpg)
 
 ☉ *Here, you will find a list of all the available commands.*
 
